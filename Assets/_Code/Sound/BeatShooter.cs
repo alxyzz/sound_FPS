acfbd8be308d48150a9a8f1c687b546bfe6578c5
alxyzz <93402865+alxyzz@users.noreply.github.com>
2023-02-03 20:08:29 +0100

    Update BeatShooter.cs

--- Assets/_Code/Sound/BeatShooter.cs
+++ Assets/_Code/Sound/BeatShooter.cs
@@ -5,32 +5,32 @@ using FMODUnity;
 
 public class BeatShooter : MonoBehaviour
 {
-    public StudioEventEmitter emitter;
+//    public StudioEventEmitter emitter;
 
     
-    public float headroom = 0.02f;
+//    public float headroom = 0.02f;
 
    
-    public PlayerShooter playerShooter;
+//    public PlayerShooter playerShooter;
 
-    void Update()
-    {
+//    void Update()
+//    {
         
-        float beatTime;
-        emitter.EventInstance.getTimelinePosition(out beatTime);
+//        float beatTime;
+//        emitter.EventInstance.getTimelinePosition(out beatTime);
 
         
-        if (beatTime % 1 < headroom || beatTime % 1 > (1 - headroom))
-        {
+//        if (beatTime % 1 < headroom || beatTime % 1 > (1 - headroom))
+//        {
             
-            playerShooter.canShoot = true;
-        }
-        else
-        {
+//            playerShooter.canShoot = true;
+//        }
+//        else
+//        {
             
-            playerShooter.canShoot = false;
-        }
-    }
-}
+//            playerShooter.canShoot = false;
+//        }
+//    }
+//}
 
 }
